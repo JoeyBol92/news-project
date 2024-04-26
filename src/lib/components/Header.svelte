5c8a14e0815d5b732870f424a5a3908e51b71351
JoeyBol <jlwbol92@gmail.com>
2024-04-26 09:48:04 +0200

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,7 @@
 <script>
-    let open = false;
+	let open = false;
 </script>
 
 <div class="border-b-[1px] border-[#E0E5E6]">
-    <div class="max-w-7xl max-[768px]:px-[25px ] mx-auto">
-        Test
-    </div>
+	<div class="max-w-7xl max-[768px]:px-[25px ] mx-auto">Test</div>
 </div>
